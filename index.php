2e101a8ba8bc0720b6d3ec28b809879f71550715
prasiddha983990 <983990@win.edu.au>
2024-09-24 19:46:26 +1000

    Updated index.php

--- index.php
+++ index.php
@@ -1,78 +1,68 @@
 <?php include 'header.php'; ?>
 
+<head>
+  <!-- Swiper JS CSS -->
+<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
+<!-- Swiper JS -->
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+</head>
+
 <div class="homepage">
     <h1>Book Your Event, Just Like That.</h1>
+    <div class="swiper-container">
+        <!-- Swiper Wrapper -->
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <a href="#">
+                    <img src="resources/deadpool.jpg" alt="Event 1">
+                    <div class="text">Deadpool & Wolverine <br><br> On 26 September</div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="#">
+                    <img src="resources/gallery1.jpg" alt="Event 2">
+                    <div class="text">Random Event<br><br> On 26 September</div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="#">
+                    <img src="resources/gallery5.jpg" alt="Event 3">
+                    <div class="text">Random Event<br><br> On 26 September</div>
+                </a>
+            </div>
+            <div class="swiper-slide">
+                <a href="#">
+                    <img src="resources/coldplay.jpg" alt="Event 4">
+                    <div class="text">Coldplay Concert <br><br>Olympic Park Stadium <br><br>On 28 September</div>
+                </a>
+            </div>
+        </div>
+        <!-- Swiper Navigation -->
+        <div class="swiper-button-prev prev"></div>
+            <div class="swiper-button-next next"></div>
 
-    <!-- Slideshow container -->
-    <div class="slideshow-container">
-        <div class="slides">
-			<a href="#">
-            	<img src="resources/deadpool.jpg" alt="Event 1">
-            	<div class="text">Deadpool & Wolverine <br><br> On 26 September</div>
-			</a>
-        </div>
-		<div class="slides">
-			<a href="#">
-            	<img src="resources/gallery1.jpg" alt="Event 2">
-            	<div class="text">Random Event<br><br> On 26 September</div>
-			</a>
-        </div>
-		<div class="slides">
-			<a href="#">
-            	<img src="resources/gallery5.jpg" alt="Event 3">
-            	<div class="text">Random Event<br><br> On 26 September</div>
-			</a>
-        </div>
-        <div class="slides">
-			<a href="#">
-            	<img src="resources/coldplay.jpg" alt="Event 4">
-            	<div class="text">Coldplay Concert <br><br>Olympic Park Stadium <br><br>On 28 September</div>
-			</a>
-        </div>
+    </div> 
 
-        <!-- Navigation buttons -->
-        <a class="prev" onclick="plusSlides(-1)"><i class="fa-solid fa-angle-left"></i></a>
-        <a class="next" onclick="plusSlides(1)"><i class="fa-solid fa-angle-right"></i></a>
-    </div>
+    <p><br>BookEvent is your go-to platform for discovering and booking the best events in town. Whether it's movies, concerts, workshops, or conferences, we make it easy for you to find and secure your spot at the most exciting happenings. Browse, book, and enjoy—all in one place!</p>
+</div> 
 
-	<p><br>BookEvent is your go-to platform for discovering and booking the best events in town. Whether it's movies, concerts, workshops, or conferences, we make it easy for you to find and secure your spot at the most exciting happenings. Browse, book, and enjoy—all in one place!</p>
-</div>
+
 <script>
-	let slideIndex = 0;
-let slides = document.getElementsByClassName("slides");
-let totalSlides = slides.length;
-let slideTimer; // Store the timer reference
-
-function showSlides(n) {
-    // Reset slideIndex if out of bounds
-    if (n >= totalSlides) {
-        slideIndex = 0;
-    } else if (n < 0) {
-        slideIndex = totalSlides - 1;
-    } else {
-        slideIndex = n;
-    }
-
-    // Apply the sliding effect by adjusting the transform property
-    for (let i = 0; i < totalSlides; i++) {
-        slides[i].style.transform = `translateX(${-(slideIndex * 100)}%)`;
-    }
-
-    // Reset and restart the timer after slide change
-    clearTimeout(slideTimer);
-    slideTimer = setTimeout(() => showSlides(slideIndex + 1), 4000);
-}
-
-// Function to move to the next or previous slide when icons are clicked
-function plusSlides(n) {
-    showSlides(slideIndex + n);
-}
-
-// Start automatic sliding
-slideTimer = setTimeout(() => showSlides(slideIndex + 1), 4000);
-
+    var swiper = new Swiper('.swiper-container', {
+        loop: true, // Allows continuous looping
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 3000, // Slide change every 3 seconds
+            disableOnInteraction: false, // Keep autoplay even after user interaction
+        },
+        speed: 1500, // Speed of transition between slides
+        slidesPerView: 1, // One slide at a time
+        spaceBetween: 0, // No space between slides
+    });
 </script>
 
-	<br><br><br>
 
 <?php include 'footer.php'; ?>
